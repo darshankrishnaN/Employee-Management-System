ff6767a146a58d1a2ea4bf575958a6d7a1fa94e6
Darshan Krishna N <143327911+darshankrishnaN@users.noreply.github.com>
2024-12-05 18:35:29 +0530

    Delete Employee.sql

--- Employee.sql
+++
@@ -1,9 +0,0 @@
-CREATE TRIGGER AfterSalaryUpdate
-AFTER UPDATE ON Salaries
-FOR EACH ROW
-BEGIN
-    INSERT INTO SalaryLog (salary_id, old_base_salary, new_base_salary, updated_at)
-    VALUES (NEW.salary_id, OLD.base_salary, NEW.base_salary, NOW())
-
-END;
-
